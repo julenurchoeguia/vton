2e4044370e778ac51521bd241dfc68f0473ef145
Daniel Rodriguez <darc2203@gmail.com>
2024-02-08 16:53:11 +0000

    feat: VAE in refiners framework

--- src/vae.py
+++ src/vae.py
@@ -51,15 +51,15 @@ class Encoder(fl.Chain):
             MaxPool2d(2),
             Resblock(128, 256),
             MaxPool2d(2),
-            fl.Conv2d(256, 8, 1, padding=0),
+            fl.Conv2d(256, 32, 1, padding=0),
             Flatten(),
         )
 
 class Decoder(fl.Chain):
     def __init__(self, output_channels: int = 3):
         super().__init__(
-            Unflatten(dim=0, sizes=torch.Size([1, 8, 128, 96])),
-            fl.Conv2d(8, 256, 1, padding=0),
+            Unflatten(dim=0, sizes=torch.Size([1, 32, 128, 96])),
+            fl.Conv2d(32, 256, 1, padding=0),
             Resblock(256, 128),
             fl.Upsample(channels=128,upsample_factor=2),
             Resblock(128, 64),
@@ -70,34 +70,36 @@ class Decoder(fl.Chain):
             fl.Conv2d(output_channels, output_channels, 1, padding=0),
         )
 
-class VAE(fl.Module):
-    def __init__(self,latent_dim) -> None:
+class NormalRepametrization(fl.Module):
+    def __init__(self):
         super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-        self.fc1 = fl.Linear(8*128*96, latent_dim)
-        self.fc2 = fl.Linear(8*128*96, latent_dim)
-        self.fc3 = fl.Linear(latent_dim, 8*128*96)
-    
-    def reparameterize(self, mu, logvar):
+
+    def forward(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
         eps = torch.randn(*mu.size()).to(device)
         z = mu + std * eps
         return z
-    
-    def bottleneck(self, h):
-        mu, logvar = self.fc1(h), self.fc2(h)
-        z = self.reparameterize(mu, logvar)
-        return z, mu, logvar
-    
-    def representation(self, x):
-        return self.bottleneck(self.encoder(x))[0]
-    
-    def forward(self, x):
-        h = self.encoder(x)
-        z, mu, logvar = self.bottleneck(h)
-        z = self.fc3(z)
-        return self.decoder(z), mu, logvar
+
+class VAE(fl.Chain):
+    def __init__(self,latent_dim) -> None:
+        super().__init__(
+            Encoder(),
+            fl.Parallel(
+                fl.Linear(32*128*96, latent_dim), 
+                fl.Linear(32*128*96, latent_dim)
+            ),
+            fl.Parallel(
+                fl.Chain(
+                    NormalRepametrization(),
+                    fl.Linear(latent_dim, 32*128*96),
+                    Decoder(),
+                ),
+                fl.Distribute(
+                    fl.Identity(),
+                    fl.Identity(),
+                )    
+            )
+        )
     
 
 
--- src/vae_trainer.py
+++ src/vae_trainer.py
@@ -18,9 +18,77 @@ from refiners.fluxion.utils import tensor_to_image
 from refiners.training_utils.config import BaseConfig
 from vae import VAE
 from image_dataset import VAEBatch, ImageDataset
+import math
 
 seed = 42
 seed_everything(seed)
+
+class Annealer:
+    """
+    This class is used to anneal the KL divergence loss over the course of training VAEs.
+    After each call, the step() function should be called to update the current epoch.
+    """
+
+    def __init__(self, total_steps, shape, baseline=0.0, cyclical=False, disable=False):
+        """
+        Parameters:
+            total_steps (int): Number of epochs to reach full KL divergence weight.
+            shape (str): Shape of the annealing function. Can be 'linear', 'cosine', or 'logistic'.
+            baseline (float): Starting value for the annealing function [0-1]. Default is 0.0.
+            cyclical (bool): Whether to repeat the annealing cycle after total_steps is reached.
+            disable (bool): If true, the __call__ method returns unchanged input (no annealing).
+        """
+        self.total_steps = total_steps
+        self.current_step = 0
+        self.cyclical = cyclical
+        self.shape = shape
+        self.baseline = baseline
+        if disable:
+            self.shape = 'none'
+            self.baseline = 0.0
+
+    def __call__(self, kld):
+        """
+        Args:
+            kld (torch.tensor): KL divergence loss
+        Returns:
+            out (torch.tensor): KL divergence loss multiplied by the slope of the annealing function.
+        """
+        out = kld * self.slope()
+        return out
+
+    def slope(self):
+        if self.shape == 'linear':
+            y = (self.current_step / self.total_steps)
+        elif self.shape == 'cosine':
+            y = (math.cos(math.pi * (self.current_step / self.total_steps - 1)) + 1) / 2
+        elif self.shape == 'logistic':
+            exponent = ((self.total_steps / 2) - self.current_step)
+            y = 1 / (1 + math.exp(exponent))
+        elif self.shape == 'none':
+            y = 1.0
+        else:
+            raise ValueError('Invalid shape for annealing function. Must be linear, cosine, or logistic.')
+        y = self.add_baseline(y)
+        return y
+
+    def step(self):
+        if self.current_step < self.total_steps:
+            self.current_step += 1
+        if self.cyclical and self.current_step >= self.total_steps:
+            self.current_step = 0
+        return
+
+    def add_baseline(self, y):
+        y_out = y * (1 - self.baseline) + self.baseline
+        return y_out
+
+    def cyclical_setter(self, value):
+        if value is not bool:
+            raise ValueError('Cyclical_setter method requires boolean argument (True/False)')
+        else:
+            self.cyclical = value
+        return
 
 class VAEConfig(BaseConfig):
     path_dataset_train : str
@@ -29,9 +97,13 @@ class VAEConfig(BaseConfig):
 
 class VAETrainer(Trainer[VAEConfig, VAEBatch]):
 
+    def __init__(self, config: VAEConfig):
+        super().__init__(config)
+        self.annealer = Annealer(total_steps=self.clock.num_steps, shape='linear')
+
     @cached_property # pour ne pas loader autoencoder à chaque fois
     def vae(self):
-        return VAE(latent_dim=32).to(device=self.device)
+        return VAE(latent_dim=2**8).to(device=self.device)
 
     def load_models(self) -> dict[str, fl.Module]:
         return {"vae": self.vae}
@@ -46,8 +118,11 @@ class VAETrainer(Trainer[VAEConfig, VAEBatch]):
 
     def compute_loss(self, batch: VAEBatch) -> Tensor:
         image = batch.to(device=self.device)
-        y,mu,logvar = self.vae(image)
-        loss = (y-image).norm() + 0.5 * torch.sum(logvar.exp() + mu.pow(2) - 1 - logvar)
+        y,normal_dist = self.vae(image)
+        mu, logvar = normal_dist
+        mse = 1000 * mse_loss(y,image)
+        kld = - 0.5 * torch.mean(1 + logvar - mu ** 2 - logvar.exp())
+        loss = mse + kld
         return loss
     
     def compute_evaluation(self) -> None:
@@ -58,8 +133,11 @@ class VAETrainer(Trainer[VAEConfig, VAEBatch]):
         for k, image in enumerate(dataset_val):
             image = image.unsqueeze(0)
             image = image.to(device=self.device)
-            y,mu,logvar  = self.vae(image)
-            loss = (y-image).norm() + 0.5 * torch.sum(logvar.exp() + mu.pow(2) - 1 - logvar)
+            y,normal_dist = self.vae(image)
+            mu, logvar = normal_dist
+            mse = 1000 * mse_loss(y,image)
+            kld = - 0.5 * torch.mean(1 + logvar - mu ** 2 - logvar.exp())
+            loss = mse + kld
             loss_val += loss
         
             if k < 20:
