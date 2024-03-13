00f979d55a15c2ad393ccbe6ad4d285f9399ee33
Daniel Rodriguez <darc2203@gmail.com>
2024-03-13 19:28:27 +0100

    merge with main

--- src/models/vae.py
+++ src/models/vae.py
@@ -10,32 +10,35 @@ from models.architecture_utils import (
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-class VAE(fl.Module):
-    def __init__(self,latent_dim) -> None:
+class NormalRepametrization(fl.Module):
+    def __init__(self):
         super().__init__()
-        self.encoder = Encoder()
-        self.decoder = Decoder()
-        self.fc1 = fl.Linear(8*128*96, latent_dim)
-        self.fc2 = fl.Linear(8*128*96, latent_dim)
-        self.fc3 = fl.Linear(latent_dim, 8*128*96)
-    
-    def reparameterize(self, mu, logvar):
+
+    def forward(self, mu, logvar):
         std = logvar.mul(0.5).exp_()
         eps = torch.randn(*mu.size()).to(device)
         z = mu + std * eps
         return z
+
+class VAE(fl.Chain):
+    def __init__(self,latent_dim) -> None:
+        super().__init__(
+            Encoder(),
+            fl.Parallel(
+                fl.Linear(32*128*96, latent_dim), 
+                fl.Linear(32*128*96, latent_dim)
+            ),
+            fl.Parallel(
+                fl.Chain(
+                    NormalRepametrization(),
+                    fl.Linear(latent_dim, 32*128*96),
+                    Decoder(),
+                ),
+                fl.Distribute(
+                    fl.Identity(),
+                    fl.Identity(),
+                )    
+            )
+        )
     
-    def bottleneck(self, h):
-        mu, logvar = self.fc1(h), self.fc2(h)
-        z = self.reparameterize(mu, logvar)
-        return z, mu, logvar
-    
-    def representation(self, x):
-        return self.bottleneck(self.encoder(x))[0]
-    
-    def forward(self, x):
-        h = self.encoder(x)
-        z, mu, logvar = self.bottleneck(h)
-        z = self.fc3(z)
-        return self.decoder(z), mu, logvar
-    
+
