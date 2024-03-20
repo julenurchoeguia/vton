24d862ab9263a7c7013755052d55641d77867eda
Daniel Rodriguez <darc2203@gmail.com>
2024-03-20 09:50:14 +0100

    feat(scm to refiners): NAFBlock

--- src/models/scm.py
+++ src/models/scm.py
@@ -16,104 +16,138 @@ Simple Baselines for Image Restoration
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from refiners.fluxion import layers as fl
+from src.models.architecture_utils import Dropout
 
-class LayerNormFunction(torch.autograd.Function):
 
-    @staticmethod
-    def forward(ctx, x, weight, bias, eps):
-        ctx.eps = eps
-        N, C, H, W = x.size()
-        mu = x.mean(1, keepdim=True)
-        var = (x - mu).pow(2).mean(1, keepdim=True)
-        y = (x - mu) / (var + eps).sqrt()
-        ctx.save_for_backward(y, var, weight)
-        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
-        return y
+class SimpleGate(fl.Module):
+    '''
+    SimpleGate is a simplified version of the Squeeze-and-Excitation (SE) block.
+    '''
+    def __init__(self) -> None:
+        super().__init__()
 
-    @staticmethod
-    def backward(ctx, grad_output):
-        eps = ctx.eps
-
-        N, C, H, W = grad_output.size()
-        y, var, weight = ctx.saved_variables
-        g = grad_output * weight.view(1, C, 1, 1)
-        mean_g = g.mean(dim=1, keepdim=True)
-
-        mean_gy = (g * y).mean(dim=1, keepdim=True)
-        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
-        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(
-            dim=0), None
-
-class LayerNorm2d(nn.Module):
-
-    def __init__(self, channels, eps=1e-6):
-        super(LayerNorm2d, self).__init__()
-        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
-        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
-        self.eps = eps
-
-    def forward(self, x):
-        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
-
-class SimpleGate(nn.Module):
-    def forward(self, x):
+    def forward(self,x: torch.Tensor) -> torch.Tensor:
         x1, x2 = x.chunk(2, dim=1)
         return x1 * x2
 
-class NAFBlock(nn.Module):
-    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+class AdaptiveAvgPool2d(nn.AdaptiveAvgPool2d, fl.WeightedModule):
+    def __init__(self, output_size):
+        super().__init__(output_size)
+
+class ElementwiseMultiply(fl.Module):
+    def __init__(self) -> None:
         super().__init__()
-        dw_channel = c * DW_Expand
-        self.conv1 = nn.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv2 = nn.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
-                               bias=True)
-        self.conv3 = nn.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        
-        # Simplified Channel Attention
-        self.sca = nn.Sequential(
-            nn.AdaptiveAvgPool2d(1),
-            nn.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
-                      groups=1, bias=True),
+
+    def forward(self, x: torch.Tensor, y: torch.Tensor) -> torch.Tensor:
+        return x * y
+
+
+class SimplifiedChannelAttention(fl.Chain):
+
+    def __init__(self,dw_channel) -> None:
+        super().__init__(
+            fl.Parallel(
+                fl.Chain(
+                    AdaptiveAvgPool2d(1),
+                    fl.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+                            groups=1, bias=True),
+                ),
+                fl.Identity()
+            ),
+            ElementwiseMultiply()
         )
 
-        # SimpleGate
-        self.sg = SimpleGate()
+class ParametersMultiply(fl.Module):
+    def __init__(self, params : fl.Parameter) -> None:
+        super().__init__()
+        self.params = params
 
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return x * self.params
+
+class NAFBlock(fl.Chain):
+    
+    def __init__(self, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        
+        dw_channel = c * DW_Expand
         ffn_channel = FFN_Expand * c
-        self.conv4 = nn.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-        self.conv5 = nn.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        super().__init__(
+            fl.Sum(
+                fl.Chain(
+                    fl.LayerNorm2d(c),
+                    fl.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
+                    fl.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,bias=True),
+                    SimpleGate(),
+                    SimplifiedChannelAttention(dw_channel),
+                    fl.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
+                    Dropout(probability=drop_out_rate),
+                    ParametersMultiply(fl.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)),
+                ),
+                fl.Identity(),
+            ),
+            fl.Sum(
+                fl.Chain(
+                    fl.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
+                    SimpleGate(),
+                    fl.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
+                    Dropout(probability=drop_out_rate),
+                    ParametersMultiply(fl.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)),
+                ),
+                fl.Identity(),
+            )
+        )
+        
+    #     self.conv1 = fl.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+    #     self.conv2 = fl.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
+    #                            bias=True)
+    #     self.conv3 = fl.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+        
+    #     # Simplified Channel Attention
+    #     self.sca = fl.Chain(
+    #         AdaptiveAvgPool2d(1),
+    #         fl.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
+    #                   groups=1, bias=True),
+    #     )
 
-        self.norm1 = LayerNorm2d(c)
-        self.norm2 = LayerNorm2d(c)
+    #     # SimpleGate
+    #     self.sg = SimpleGate()
 
-        self.dropout1 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
-        self.dropout2 = nn.Dropout(drop_out_rate) if drop_out_rate > 0. else nn.Identity()
+        
+    #     self.conv4 = fl.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+    #     self.conv5 = fl.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
 
-        self.beta = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
-        self.gamma = nn.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+    #     self.norm1 = fl.LayerNorm2d(c)
+    #     self.norm2 = fl.LayerNorm2d(c)
 
-    def forward(self, inp):
-        x = inp
+    #     self.dropout1 = Dropout(probability=drop_out_rate) 
+    #     self.dropout2 = Dropout(probability=drop_out_rate) 
 
-        x = self.norm1(x)
+    #     self.beta = fl.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
+    #     self.gamma = fl.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)
 
-        x = self.conv1(x)
-        x = self.conv2(x)
-        x = self.sg(x)
-        x = x * self.sca(x)
-        x = self.conv3(x)
+    # def forward(self, inp):
+    #     x = inp
 
-        x = self.dropout1(x)
+    #     x = self.norm1(x)
 
-        y = inp + x * self.beta
+    #     x = self.conv1(x)
+    #     x = self.conv2(x)
+    #     x = self.sg(x)
+    #     x = x * self.sca(x)
+    #     x = self.conv3(x)
 
-        x = self.conv4(self.norm2(y))
-        x = self.sg(x)
-        x = self.conv5(x)
+    #     x = self.dropout1(x)
 
-        x = self.dropout2(x)
+    #     y = inp + x * self.beta
 
-        return y + x * self.gamma
+    #     x = self.conv4(self.norm2(y))
+    #     x = self.sg(x)
+    #     x = self.conv5(x)
+
+    #     x = self.dropout2(x)
+
+    #     return y + x * self.gamma
 
 class NAFNet(nn.Module):
 
