d453eb0846d1e7798da7e634146b3ad8485cdd1d
Daniel Rodriguez <darc2203@gmail.com>
2024-03-20 18:23:26 +0100

    feat(scm-to-refiners): first version

--- src/models/scm.py
+++ src/models/scm.py
@@ -17,8 +17,21 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from refiners.fluxion import layers as fl
+from refiners.fluxion.context import Contexts
 from src.models.architecture_utils import Dropout
+from torch.nn import PixelShuffle as _PixelShuffle
+from typing import Iterable, cast
 
+
+
+
+
+class PixelShuffle(_PixelShuffle, fl.Module):
+    """Pixel Shuffle layer.
+    """
+
+    def __init__(self, upscale_factor: int):
+        _PixelShuffle.__init__(self, upscale_factor=upscale_factor)
 
 class SimpleGate(fl.Module):
     '''
@@ -31,7 +44,7 @@ class SimpleGate(fl.Module):
         x1, x2 = x.chunk(2, dim=1)
         return x1 * x2
 
-class AdaptiveAvgPool2d(nn.AdaptiveAvgPool2d, fl.WeightedModule):
+class AdaptiveAvgPool2d(nn.AdaptiveAvgPool2d, fl.Module):
     def __init__(self, output_size):
         super().__init__(output_size)
 
@@ -51,20 +64,12 @@ class SimplifiedChannelAttention(fl.Chain):
                 fl.Chain(
                     AdaptiveAvgPool2d(1),
                     fl.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
-                            groups=1, bias=True),
+                            groups=1, use_bias=True),
                 ),
                 fl.Identity()
             ),
             ElementwiseMultiply()
         )
-
-class ParametersMultiply(fl.Module):
-    def __init__(self, params : fl.Parameter) -> None:
-        super().__init__()
-        self.params = params
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return x * self.params
 
 class NAFBlock(fl.Chain):
     
@@ -73,49 +78,51 @@ class NAFBlock(fl.Chain):
         dw_channel = c * DW_Expand
         ffn_channel = FFN_Expand * c
         super().__init__(
-            fl.Sum(
-                fl.Chain(
-                    fl.LayerNorm2d(c),
-                    fl.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
-                    fl.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,bias=True),
-                    SimpleGate(),
-                    SimplifiedChannelAttention(dw_channel),
-                    fl.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
-                    Dropout(probability=drop_out_rate),
-                    ParametersMultiply(fl.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)),
+            fl.Residual(
+                fl.LayerNorm2d(c),
+                fl.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True),
+                fl.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,use_bias=True),
+                SimpleGate(),
+                SimplifiedChannelAttention(dw_channel),
+                fl.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True),
+                Dropout(probability=drop_out_rate),
+                fl.Parallel(
+                    fl.Parameter(c, 1, 1),
+                    fl.Identity(),
                 ),
-                fl.Identity(),
+                ElementwiseMultiply(),
             ),
-            fl.Sum(
-                fl.Chain(
-                    fl.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
-                    SimpleGate(),
-                    fl.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True),
-                    Dropout(probability=drop_out_rate),
-                    ParametersMultiply(fl.Parameter(torch.zeros((1, c, 1, 1)), requires_grad=True)),
+            fl.Residual(
+                fl.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True),
+                SimpleGate(),
+                fl.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True),
+                Dropout(probability=drop_out_rate),
+                fl.Parallel(
+                    fl.Parameter(c, 1, 1),
+                    fl.Identity(),
                 ),
-                fl.Identity(),
+                ElementwiseMultiply(),
             )
         )
         
-    #     self.conv1 = fl.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+    #     self.conv1 = fl.Conv2d(in_channels=c, out_channels=dw_channel, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True)
     #     self.conv2 = fl.Conv2d(in_channels=dw_channel, out_channels=dw_channel, kernel_size=3, padding=1, stride=1, groups=dw_channel,
-    #                            bias=True)
-    #     self.conv3 = fl.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+    #                            use_bias=True)
+    #     self.conv3 = fl.Conv2d(in_channels=dw_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True)
         
     #     # Simplified Channel Attention
     #     self.sca = fl.Chain(
     #         AdaptiveAvgPool2d(1),
     #         fl.Conv2d(in_channels=dw_channel // 2, out_channels=dw_channel // 2, kernel_size=1, padding=0, stride=1,
-    #                   groups=1, bias=True),
+    #                   groups=1, use_bias=True),
     #     )
 
     #     # SimpleGate
     #     self.sg = SimpleGate()
 
         
-    #     self.conv4 = fl.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
-    #     self.conv5 = fl.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, bias=True)
+    #     self.conv4 = fl.Conv2d(in_channels=c, out_channels=ffn_channel, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True)
+    #     self.conv5 = fl.Conv2d(in_channels=ffn_channel // 2, out_channels=c, kernel_size=1, padding=0, stride=1, groups=1, use_bias=True)
 
     #     self.norm1 = fl.LayerNorm2d(c)
     #     self.norm2 = fl.LayerNorm2d(c)
@@ -155,9 +162,9 @@ class NAFNet(nn.Module):
         super().__init__()
 
         self.intro = nn.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
+                              use_bias=True)
         self.ending = nn.Conv2d(in_channels=width, out_channels=3, kernel_size=3, padding=1, stride=1, groups=1,
-                              bias=True)
+                              use_bias=True)
 
         self.encoders = nn.ModuleList()
         self.decoders = nn.ModuleList()
@@ -185,7 +192,7 @@ class NAFNet(nn.Module):
         for num in dec_blk_nums:
             self.ups.append(
                 nn.Sequential(
-                    nn.Conv2d(chan, chan * 2, 1, bias=False),
+                    nn.Conv2d(chan, chan * 2, 1, use_bias=False),
                     nn.PixelShuffle(2)
                 )
             )
@@ -259,3 +266,158 @@ class NAFNet_Combine(NAFNet):
 
         return x[:, :, :H, :W]
 
+
+class NAFBlockEnc(fl.Chain):
+
+    def __init__(self, nb_blocks, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__(
+            *[
+                NAFBlock(c, DW_Expand, FFN_Expand, drop_out_rate) for _ in range(nb_blocks)
+            ]
+        )
+
+class NAFBlockDec(fl.Chain):
+
+    def __init__(self, nb_blocks, c, DW_Expand=2, FFN_Expand=2, drop_out_rate=0.):
+        super().__init__(
+            *[
+                NAFBlock(c, DW_Expand, FFN_Expand, drop_out_rate) for _ in range(nb_blocks)
+            ]
+        )
+
+class DownBlocks(fl.Chain):
+    def __init__(
+        self,
+        in_channels: int,
+        enc_blk_nums: Iterable[int] = [2, 2, 4, 8],
+    ):
+        self.in_channels_list = [in_channels * (2 ** i) for i in range(len(enc_blk_nums)+1)] 
+        super().__init__(
+            *[
+                fl.Chain(
+                    ## Encoding Blocks
+                    NAFBlockEnc(enc_blk_num, self.in_channels_list[index]),
+                    ## Downsampling Block
+                    fl.Conv2d(in_channels=self.in_channels_list[index], out_channels=self.in_channels_list[index+1], kernel_size=2, stride=2)
+                )
+                for index, enc_blk_num in enumerate(enc_blk_nums)
+            ]
+        )
+
+class UpBlocks(fl.Chain):
+    def __init__(
+        self,
+        in_channels: int,
+        dec_blk_nums: Iterable[int] = [2, 2, 2, 2],
+    ) -> None:
+
+        self.in_channels_list = [in_channels // (2 ** i) for i in range(len(dec_blk_nums)+1)]
+        super().__init__(
+            *[
+                fl.Chain(
+                    ## Upsampling Block
+                    fl.Chain(
+                        fl.Conv2d(in_channels=self.in_channels_list[index], out_channels=self.in_channels_list[index+1]*4, kernel_size=1,use_bias=False),
+                        PixelShuffle(2)
+                    ),
+                    ## Decoding Blocks
+                    NAFBlockDec(dec_blk_num, self.in_channels_list[index+1])
+                )
+                for index, dec_blk_num in enumerate(dec_blk_nums)
+            ]
+        )
+
+class MiddleBlock(fl.Chain):
+    def __init__(
+            self, 
+            in_channels: int ,
+            nb_middle_blocks: int = 1
+        ) -> None:
+        super().__init__(
+            *[
+                NAFBlock(in_channels) for _ in range(nb_middle_blocks)
+            ]
+        )
+
+class EncoderAccumulator(fl.Passthrough):
+    def __init__(self, n: int) -> None:
+        self.n = n
+        super().__init__(
+            fl.SetContext(context="unet", key="naf_block_encoders", callback=self.update),
+        )
+
+    def update(self, naf_block_encoder: list[torch.Tensor | float], x: torch.Tensor) -> None:
+        naf_block_encoder[self.n] = x
+
+class DecoderAdditionner(fl.Chain):
+    def __init__(self, n: int) -> None:
+        self.n = n
+
+        super().__init__(
+            fl.Sum(
+                fl.Identity(),
+                fl.UseContext(context="unet", key="naf_block_encoders").compose(lambda naf_block_encoder: naf_block_encoder[self.n]),
+            ),
+        )
+
+    def forward(self, *args: torch.Any) -> torch.Any:
+        return super().forward(*args)
+
+class NAFNet_UNet(fl.Chain):
+    """NAFNet U-Net.
+
+    This U-Net is based on the NAFNet architecture.
+    """
+
+    def __init__(
+        self, in_channels=16, middle_blk_num=12, enc_blk_nums=[2, 2, 4, 8], dec_blk_nums=[2, 2, 2, 2]
+    ) -> None:
+        """Initialize the U-Net.
+
+        Args:
+            in_channels: The number of input channels.
+            device: The PyTorch device to use for computation.
+            dtype: The PyTorch dtype to use for computation.
+        """
+        self.in_channels = in_channels
+        self.mid_channels = in_channels * (2 ** 4)
+        super().__init__(
+            DownBlocks(in_channels=self.in_channels, enc_blk_nums=enc_blk_nums),
+            MiddleBlock(in_channels=self.mid_channels, nb_middle_blocks=middle_blk_num),
+            UpBlocks(in_channels=self.mid_channels, dec_blk_nums=dec_blk_nums),
+        )
+
+        for n, block in enumerate(cast(Iterable[fl.Chain], self.DownBlocks)):
+            # Inject an accumulator after each encoding block.
+            block.insert(1, EncoderAccumulator(n))
+        for n, block in enumerate(cast(Iterable[fl.Chain], self.UpBlocks)):
+            # Inject an additioner before each decoding block.
+            block.insert(1, DecoderAdditionner(3-n))
+
+    def init_context(self) -> Contexts:
+        return {
+            "unet": {"naf_block_encoders": [0.0] * 4},
+        }
+    
+class SCM(fl.Sum):
+    def __init__(
+        self, img_channel=6, width=16, middle_blk_num=12, enc_blk_nums=[2, 2, 4, 8], dec_blk_nums=[2, 2, 2, 2], scm_weight=1.0
+    ) -> None:
+        super().__init__(
+            fl.Chain(
+                fl.Conv2d(in_channels=img_channel, out_channels=width, kernel_size=3, padding=1, stride=1, groups=1,use_bias=True),
+                NAFNet_UNet(in_channels=width, middle_blk_num=middle_blk_num, enc_blk_nums=enc_blk_nums, dec_blk_nums=dec_blk_nums),
+                fl.Conv2d(in_channels=width, out_channels=3, kernel_size=3, padding=1, stride=1, groups=1,use_bias=True),              
+            ),
+            fl.Chain(
+                fl.Slicing(dim=1, start=3, end=6),
+                fl.Multiply(scale = scm_weight),
+            )
+        )
+
+
+if __name__ == "__main__":
+    model = SCM()
+    x = torch.randn(1, 6, 1024, 1024)
+    y = model(x)
+    print(y.shape)
